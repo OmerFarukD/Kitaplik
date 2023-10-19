62cca4722388ee9c1d3a89215d9cb62015aca8d2
omerfarukkdogan <omerc@DESKTOP-P7FDVCL>
2023-10-19 13:16:55 +0300

    Add project files.

---
+++ Kitaplik/Repository/RepositoryBaglantisi.cs
@@ -0,0 +1,18 @@
+﻿using Kitaplik.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kitaplik.Repository;
+public class RepositoryBaglantisi : DbContext
+{
+    public RepositoryBaglantisi(DbContextOptions options) : base(options)
+    {
+        
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB;Database=Kitaplik_db;Trusted_Connection=true");
+    }
+    public DbSet<Book> Books { get; set; }
+
+}
